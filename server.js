a46070f892794da5f93e3e01ebf471fed0dac235
ghabriel <51523197+capivarista@users.noreply.github.com>
2025-06-10 15:30:02 -0300

    ajustes server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'antonio77',
+    password: '',
     database: 'duckshot'
 });
 
